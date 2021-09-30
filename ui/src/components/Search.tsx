cb28af83b59399d1362c506765543b0bd38a5db4
Alex Sparrow <alex@quartic.io>
2021-09-30 13:00:33 +0100

    Improve ergonomics a bit

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -50,64 +50,80 @@ export const Search = () => {
     },
   });
 
+  const results = _.take(data?.data?.search, 10);
+
+  console.log(search);
+
   return (
     <Popover as="div" className="relative inline-block text-left w-full">
-      <div className="relative text-indigo-200 focus-within:text-gray-400">
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <SearchIcon className="h-5 w-5" aria-hidden="true" />
-        </div>
-        <input
-          id="search"
-          name="search"
-          className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-indigo-400 bg-opacity-25 text-indigo-100 placeholder-indigo-200 focus:outline-none focus:bg-white focus:ring-0 focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm"
-          placeholder="Search projects"
-          onChange={(e) => setSearch(e.target.value)}
-          type="search"
-        />
-      </div>
-
-      <Transition
-        show={open}
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Popover.Panel
-          static
-          className="origin-top-right absolute left-0 right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-        >
-          <div className="p-4">
-            {data.fetching ? (
-              <LoadingGrid />
-            ) : (
-              <div>
-                {_.take(data?.data?.search, 10).map((result) => (
-                  <div
-                    className="flex items-center py-2 px-1 cursor-pointer hover:bg-gray-200"
-                    onClick={() => {
-                      setSearch("");
-                      history.push(`/entity/${result.entityId}`);
-                    }}
-                  >
-                    <EntityIcon type={result.typeName} />
-                    <span className="ml-2">
-                      {
-                        result.facets.find(
-                          (facet: any) => facet.uri === "http://schema.org/name"
-                        )?.value
-                      }
-                    </span>
-                  </div>
-                ))}
+      {({ open: popoverOpen }) => (
+        <>
+          <Popover.Button as="div">
+            <div className="relative text-indigo-200 focus-within:text-gray-400">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <SearchIcon className="h-5 w-5" aria-hidden="true" />
               </div>
-            )}
-          </div>
-        </Popover.Panel>
-      </Transition>
+              <input
+                id="search"
+                name="search"
+                className="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-indigo-400 bg-opacity-25 text-indigo-100 placeholder-indigo-200 focus:outline-none focus:bg-white focus:ring-0 focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm"
+                value={search}
+                placeholder="Search projects"
+                onChange={(e) => setSearch(e.target.value)}
+                // type="search"
+              />
+            </div>
+          </Popover.Button>
+
+          <Transition
+            show={open && popoverOpen}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Popover.Panel
+              static
+              className="origin-top-right absolute left-0 right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              <div className="p-4">
+                {data.fetching ? (
+                  <LoadingGrid />
+                ) : (
+                  <div>
+                    {results.length > 0 ? (
+                      results.map((result) => (
+                        <div
+                          className="flex items-center py-2 px-1 cursor-pointer hover:bg-gray-200"
+                          onClick={() => {
+                            setSearch("");
+                            history.push(`/entity/${result.entityId}`);
+                          }}
+                        >
+                          <EntityIcon type={result.typeName} />
+                          <span className="ml-2">
+                            {
+                              result.facets.find(
+                                (facet: any) =>
+                                  facet.uri === "http://schema.org/name"
+                              )?.value
+                            }
+                          </span>
+                        </div>
+                      ))
+                    ) : (
+                      <div>No results :-(</div>
+                    )}
+                  </div>
+                )}
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
     </Popover>
   );
 };
