3f6a9b6457ed1d298a388cf7dc5d65221d9f6631
Alex Sparrow <alex@quartic.io>
2021-10-14 12:47:59 +0100

    Tidy sign in

--- ui/src/routes/SignIn.tsx
+++ ui/src/routes/SignIn.tsx
@@ -20,12 +20,12 @@ export const SignIn = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
+        {/* <img
           className="mx-auto h-12 w-auto"
           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
           alt="Workflow"
-        />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        /> */}
+        <h2 className="text-center text-3xl font-extrabold text-gray-900">
           Sign in to your account
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
@@ -135,7 +135,7 @@ export const SignIn = () => {
             </div>
           </form>
 
-          <div className="mt-6">
+          {/* <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300" />
@@ -145,9 +145,9 @@ export const SignIn = () => {
                   Or continue with
                 </span>
               </div>
-            </div>
+            </div> */}
 
-            <div className="mt-6 grid grid-cols-1 gap-3">
+          {/* <div className="mt-6 grid grid-cols-1 gap-3">
               <div>
                 <a
                   href="#"
@@ -169,7 +169,7 @@ export const SignIn = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
