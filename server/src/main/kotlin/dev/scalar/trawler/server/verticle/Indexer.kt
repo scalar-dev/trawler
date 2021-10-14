4cf8fdf754b59ed0c777cd25b722854a2fcd7d7e
Alex Sparrow <alex@quartic.io>
2021-10-14 17:09:48 +0100

    Add some exception handling around indexer

--- server/src/main/kotlin/dev/scalar/trawler/server/verticle/Indexer.kt
+++ server/src/main/kotlin/dev/scalar/trawler/server/verticle/Indexer.kt
@@ -34,6 +34,83 @@ class Indexer : BaseVerticle() {
             .associate { it[Entity.urn] to it[Entity.id].value }
     }
 
+    suspend fun indexFacetLog(id: UUID) = newSuspendedTransaction {
+        // Grab the log
+        val facetLog = FacetLog
+            .selectForUpdate { FacetLog.id.eq(id) }
+            .first()
+
+        val facetType = OntologyCache.CACHE[facetLog[FacetLog.projectId].value]
+            .facetTypeById(facetLog[FacetLog.typeId].value)!!
+
+        // Create any entities
+        val urns = if (facetType.metaType == FacetMetaType.RELATIONSHIP) {
+            listOf(facetLog[FacetLog.entityUrn]) + facetLog[FacetLog.value]!!.map { it.toString() }
+        } else {
+            listOf(facetLog[FacetLog.entityUrn])
+        }
+
+        val entities = indexEntities(facetLog[FacetLog.projectId].value, urns)
+        val entityId = entities[facetLog[FacetLog.entityUrn]]!!
+
+        // Lookup the table
+        val facetValues = FacetValue.select {
+            FacetValue.entityId.eq(entityId)
+                .and(FacetValue.typeId.eq(facetLog[FacetLog.typeId].value))
+        }.toList()
+
+        if (facetType.indexTimeSeries) {
+            FacetTimeSeries.insertIgnore {
+                it[FacetTimeSeries.entityId] = entityId
+                it[FacetTimeSeries.typeId] = facetType.id
+                it[FacetTimeSeries.version] = facetLog[FacetLog.version]
+                it[FacetTimeSeries.timestamp] = facetLog[FacetLog.timestamp] ?: facetLog[FacetLog.createdAt]
+
+                if (facetType.metaType == FacetMetaType.DOUBLE) {
+                    it[FacetTimeSeries.valueDouble] = (facetLog[FacetLog.value]!![0] as Number).toDouble()
+                } else if (facetType.metaType == FacetMetaType.INT) {
+                    it[FacetTimeSeries.valueLong] = (facetLog[FacetLog.value]!![0] as Number).toLong()
+                }
+            }
+        }
+
+        if (facetValues.any { it[FacetValue.version] > facetLog[FacetLog.version] }) {
+            log.info("Stale write detected: $id")
+        } else {
+            // Delete any old versions
+            FacetValue.deleteWhere {
+                FacetValue.entityId.eq(entityId)
+                    .and(FacetValue.version.less(facetLog[FacetLog.version]))
+                    .and(FacetValue.typeId.eq(facetLog[FacetLog.typeId].value))
+            }
+
+            val indexedValues = facetLog[FacetLog.value]!!.withIndex()
+
+            // Add new versions
+            FacetValue.batchInsert(indexedValues) {
+                this[FacetValue.projectId] = facetLog[FacetLog.projectId].value
+                this[FacetValue.entityId] = entityId
+                this[FacetValue.typeId] = facetType.id
+                this[FacetValue.index] = it.index.toShort()
+                this[FacetValue.version] = facetLog[FacetLog.version]
+                this[FacetValue.updatedAt] = Instant.now()
+
+                if (facetType.metaType == FacetMetaType.RELATIONSHIP) {
+                    this[FacetValue.targetEntityId] = entities[it.value.toString()]
+                } else if (facetType.metaType == FacetMetaType.TYPE_REFERENCE) {
+                    this[FacetValue.entityTypeId] = UUID.fromString(it.value as String)
+                } else {
+                    this[FacetValue.value] = it.value as Any
+                }
+            }
+        }
+
+        // Update the log
+        FacetLog.update({ FacetLog.id.eq(id) }) {
+            it[FacetLog.entityId] = entities[facetLog[FacetLog.entityUrn]]
+        }
+    }
+
     override suspend fun start() {
         super.start()
         configureDatabase()
@@ -49,81 +126,11 @@ class Indexer : BaseVerticle() {
             if (counter % 100 == 0) {
                 log.info("indexing: $counter")
             }
-            newSuspendedTransaction {
-                // Grab the log
-                val facetLog = FacetLog
-                    .selectForUpdate { FacetLog.id.eq(id) }
-                    .first()
 
-                val facetType = OntologyCache.CACHE[facetLog[FacetLog.projectId].value]
-                    .facetTypeById(facetLog[FacetLog.typeId].value)!!
-
-                // Create any entities
-                val urns = if (facetType.metaType == FacetMetaType.RELATIONSHIP) {
-                    listOf(facetLog[FacetLog.entityUrn]) + facetLog[FacetLog.value]!!.map { it.toString() }
-                } else {
-                    listOf(facetLog[FacetLog.entityUrn])
-                }
-
-                val entities = indexEntities(facetLog[FacetLog.projectId].value, urns)
-                val entityId = entities[facetLog[FacetLog.entityUrn]]!!
-
-                // Lookup the table
-                val facetValues = FacetValue.select {
-                    FacetValue.entityId.eq(entityId)
-                        .and(FacetValue.typeId.eq(facetLog[FacetLog.typeId].value))
-                }.toList()
-
-                if (facetType.indexTimeSeries) {
-                    FacetTimeSeries.insertIgnore {
-                        it[FacetTimeSeries.entityId] = entityId
-                        it[FacetTimeSeries.typeId] = facetType.id
-                        it[FacetTimeSeries.version] = facetLog[FacetLog.version]
-                        it[FacetTimeSeries.timestamp] = facetLog[FacetLog.timestamp] ?: facetLog[FacetLog.createdAt]
-
-                        if (facetType.metaType == FacetMetaType.DOUBLE) {
-                            it[FacetTimeSeries.valueDouble] = (facetLog[FacetLog.value]!![0] as Number).toDouble()
-                        } else if (facetType.metaType == FacetMetaType.INT) {
-                            it[FacetTimeSeries.valueLong] = (facetLog[FacetLog.value]!![0] as Number).toLong()
-                        }
-                    }
-                }
-
-                if (facetValues.any { it[FacetValue.version] > facetLog[FacetLog.version] }) {
-                    log.info("Stale write detected: $id")
-                } else {
-                    // Delete any old versions
-                    FacetValue.deleteWhere {
-                        FacetValue.entityId.eq(entityId)
-                            .and(FacetValue.version.less(facetLog[FacetLog.version]))
-                            .and(FacetValue.typeId.eq(facetLog[FacetLog.typeId].value))
-                    }
-
-                    val indexedValues = facetLog[FacetLog.value]!!.withIndex()
-
-                    // Add new versions
-                    FacetValue.batchInsert(indexedValues) {
-                        this[FacetValue.projectId] = facetLog[FacetLog.projectId].value
-                        this[FacetValue.entityId] = entityId
-                        this[FacetValue.typeId] = facetType.id
-                        this[FacetValue.index] = it.index.toShort()
-                        this[FacetValue.version] = facetLog[FacetLog.version]
-                        this[FacetValue.updatedAt] = Instant.now()
-
-                        if (facetType.metaType == FacetMetaType.RELATIONSHIP) {
-                            this[FacetValue.targetEntityId] = entities[it.value.toString()]
-                        } else if (facetType.metaType == FacetMetaType.TYPE_REFERENCE) {
-                            this[FacetValue.entityTypeId] = UUID.fromString(it.value as String)
-                        } else {
-                            this[FacetValue.value] = it.value as Any
-                        }
-                    }
-                }
-
-                // Update the log
-                FacetLog.update({ FacetLog.id.eq(id) }) {
-                    it[FacetLog.entityId] = entities[facetLog[FacetLog.entityUrn]]
-                }
+            try {
+                indexFacetLog(id)
+            } catch (e: Exception) {
+                log.error("Exception while trying to index facet log $id", e)
             }
         }
     }
