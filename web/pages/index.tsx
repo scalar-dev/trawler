5eff5c2fe9126d1447a0ad2ac92f844d92fd364b
Alex Sparrow <alex@alexsparrow.dev>
2021-10-22 15:43:54 +0100

    Make the link work

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -13,12 +13,12 @@ import {
 import { Footer } from "../components/Footer";
 
 const navigation = [
-  { name: "Product", href: "#" },
+  { name: "Product", href: "#product" },
   { name: "Documentation", href: "https://docs.trawler.dev" },
 ];
 
 const Screenshot = () => (
-  <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+  <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32" id="product">
     <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
       <div>
         <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
